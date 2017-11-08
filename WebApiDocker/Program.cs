14bda27090ec6ea2df43944727ac883e550459d9
Vinaya M M <vinaya.m.mitlikoppa@Pwc.com>
2017-11-08 20:20:08 +0530

    Updated the settings and docker build environment changes

--- WebApiDocker/Program.cs
+++ WebApiDocker/Program.cs
@@ -14,7 +14,15 @@ namespace WebApiDocker
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+           var host = new WebHostBuilder()
+
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseIISIntegration()
+               .UseStartup<Startup>()
+               .Build();
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
